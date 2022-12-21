24b39d2b3a163e9773c6403a1949d7ed95f97496
elnukakujo <elnukakujo@gmail.com>
2022-12-21 14:48:55 -0500

    First commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,48 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import milestone1_2.question1 as question1
+import milestone1_2.question4 as question4
+import os
+import json
 
-"""
-General template for your streamlit app. 
-Feel free to experiment with layout and adding functionality!
-Just make sure that the required functionality is included as well
-"""
+"""question1.get_play_by_play_season_gameType(year, game_id[4:6],"data_saved/play_by_play")"""
 
-st.title("INSERT APP TITLE HERE")
+st.title("Hockey Visualization App")
 
 with st.sidebar:
-    # TODO: Add input for the sidebar
-    pass
+    st.text_input("Workspace")
+    st.text_input("Model")
+    st.text_input("Version")
 
 with st.container():
-    # TODO: Add Game ID input
-    pass
+    game_id = st.text_input("Game ID", value="2016020001")
 
 with st.container():
-    # TODO: Add Game info and predictions
-    pass
+    if st.button("Ping game") and game_id:
+        match_type = game_id[4:6]
+        if match_type == "02":
+            match_type = "regular"
+        elif match_type == "03":
+            match_type = "playoff"
+        else:
+            match_type = "None"
+        
+        year = game_id[:4]
+        path = "\\data_saved\\play_by_play\\"+year+"\\"+match_type
+
+        with open(os.path.abspath(os.getcwd()) + path +"\\"+game_id+".json", 'r') as f:
+            data = json.loads(f.read())
+        teamAway = data["gameData"]["teams"]["away"]["teamName"]
+        teamHome = data["gameData"]["teams"]["home"]["teamName"]
+        
+        st.header("Game "+game_id+": "+teamAway+" vs "+teamHome)
+       
 
 with st.container():
-    # TODO: Add data used for predictions
+    st.header("Data Used for predict")
+
+    df_raw = question4.create_full_df(directory=os.path.abspath(os.getcwd()) + path)
+    df = df_raw.loc[df_raw.Game_ID==int(game_id)]  
+    st.dataframe(df)
     pass
